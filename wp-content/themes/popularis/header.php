2e670d980fea08a8f8e3f74d2d1c1276459946f1
TheJadeEmperor <benjamin_louie@drugfree.org>
2020-11-26 17:06:57 -0500

    google analytics

--- wp-content/themes/popularis/header.php
+++ wp-content/themes/popularis/header.php
@@ -4,12 +4,21 @@
         <meta charset="windows-1252">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="http://gmpg.org/xfn/11">
-        <?php wp_head(); ?>
-        <?php
+        <?php wp_head(); 
         $template_dir = get_template_directory_uri();
         ?>
+        <!-- Gallery lightbox assets -->
         <link rel="stylesheet" href="<?php echo $template_dir ?>/assets/css/lightbox.css">
         <script src="<?php echo $template_dir ?>/assets/js/lightbox.js"></script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RCKVTDHCNF"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-RCKVTDHCNF');
+        </script>
 
         <!-- G Search Console -->
 		<meta name="google-site-verification" content="K4GZC5J-LMQCLaRN9bLptbX2f89SaS2HrFXtAGWCTts" />		
@@ -19,4 +28,4 @@
         <?php wp_body_open(); ?>
         <div class="page-wrap">
             <?php get_template_part('template-parts/header/template-part', 'toparea'); ?>
-            <?php do_action('popularis_generate_header'); ?> 
+            <?php do_action('popularis_generate_header'); ?>
